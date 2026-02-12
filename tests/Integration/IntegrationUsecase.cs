4e177b2a2c1e51d6c725598ba51b87dd0e9f8b70
Daniel <danmoreira.dev@gmail.com>
2026-02-12 18:12:55 -0300

    test: refatorar testes unitarios

--- tests/Integration/IntegrationUsecase.cs
+++ tests/Integration/IntegrationUsecase.cs
@@ -13,18 +13,48 @@ public class IntegrationUsecase
     {
         var input = new TransactionReq("4", "7", 8);
 
-        var repoMock = new Mock<IDatabase>();
-        repoMock.Setup(i => i.GetByUniqueField(input.IdPayee)).ReturnsAsync(new UserEntity(input.IdPayer, "Daniel",
-            "#@#4354252", "Daniel@gmail.com", "senha123", Role.COMUM, 19, null));
-        repoMock.Setup(i => i.GetByUniqueField(input.IdPayer)).ReturnsAsync(new UserEntity(input.IdPayer, "Bea",
-            "#@332442", "Bea@gmail.com", "senha123", Role.LOGISTA, null, null));
+        var paymentRepoMock = new Mock<IPaymentRepo>();
 
-        var serviceMock = new Mock<IService>();
-        serviceMock.Setup(i =>
-                i.RequestExternalApi<AuthorizationTransactionRes>("https://util.devi.tools/api/v2/authorize"))
-            .ReturnsAsync(new AuthorizationTransactionRes("true", new AuthorizationData(false)));
+        var userRepoMock = new Mock<IUserRepo>();
+        userRepoMock.Setup(i => i.GetByUniqueField(input.IdPayer))
+            .ReturnsAsync(
+                UserEntity.Restore(
+                    input.IdPayer,
+                    "Daniel",
+                    "#@#4354252",
+                    "Daniel@gmail.com",
+                    "senha123",
+                    Role.COMUM,
+                    19,
+                    DateTime.Now
+                )
+            );
 
-        var usecase = new PicPayUC(repoMock.Object, serviceMock.Object);
+        userRepoMock.Setup(i => i.GetByUniqueField(input.IdPayee))
+            .ReturnsAsync(
+                UserEntity.Restore(
+                    input.IdPayer,
+                    "Bea",
+                    "#@332442",
+                    "Bea@gmail.com",
+                    "senha123",
+                    Role.LOGISTA,
+                    0,
+                    DateTime.Now
+                )
+            );
+
+        var httpServicesMock = new Mock<IHttpServices>();
+        httpServicesMock
+            .Setup(i =>
+                i.RequestExternalApi<MapJson.AuthorizationTransactionRes>("https://util.devi.tools/api/v2/authorize"))
+            .ReturnsAsync(new MapJson.AuthorizationTransactionRes("true", new MapJson.AuthorizationData(true)));
+
+        var usecase = new PaymentUc(
+            paymentRepoMock.Object,
+            userRepoMock.Object,
+            httpServicesMock.Object
+        );
 
         await usecase.Transaction(input);
     }
