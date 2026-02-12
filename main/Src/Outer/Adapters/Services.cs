88344907ebca48b7ae624268c3d30f92b667aaf5
Daniel <danmoreira.dev@gmail.com>
2026-02-12 18:14:03 -0300

    feat: adiciona implementação de IHttpServices

---
+++ main/Src/Outer/Adapters/Services.cs
@@ -0,0 +1,11 @@
+﻿using PicPay.Inner.Ports;
+
+namespace PicPay.Outer.Adapters;
+
+public class HttpServices(HttpClient client) : IHttpServices
+{
+    public async Task<T?> RequestExternalApi<T>(string url) where T : class
+    {
+        return await client.GetFromJsonAsync<T>(url);
+    }
+}
