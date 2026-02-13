94d5eeed7fef652c35993fdcb7bbc49a4869b045
Daniel <danmoreira.dev@gmail.com>
2026-02-13 07:48:22 -0300

    feat(andamento): adicionar implementação de IPaymentRepo

---
+++ main/Src/Outer/Persistence/Repository/PaymentRepo.cs
@@ -0,0 +1,24 @@
+﻿using PicPay.Inner.Entity;
+using PicPay.Inner.Ports;
+using PicPay.Outer.Persistence.Schemas;
+
+namespace PicPay.Outer.Persistence.Repository;
+
+public class PaymentRepo(DbConnection _db) : IPaymentRepo
+{
+    public Task<bool> ConfirmTransaction(TransactionEntity info)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task ConfirmDeposit(UserEntity user)
+    {
+        await _db.userSchema.Update(UserSchema.ToSchema(user));
+        await _db.SaveChangesAsync();
+    }
+    
+    public Task<bool> Refund(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
