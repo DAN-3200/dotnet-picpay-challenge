cd49fd192ad16341d3a48427d01eed3f67a1b80b
Daniel <danmoreira.dev@gmail.com>
2026-02-14 18:14:29 -0300

    feat: adicionar todas as dependencias no DI do sistema

--- main/Src/Program.cs
+++ main/Src/Program.cs
@@ -1,20 +1,25 @@
 using Microsoft.EntityFrameworkCore;
-using PicPay.Inner.Usecase;
-using PicPay.Outer.Http.Middlewares;
-using PicPay.Outer.Persistence;
+using PicPay.Application.Ports;
+using PicPay.Application.Usecase;
+using PicPay.Infrastructure.Adapters;
+using PicPay.Infrastructure.Http.Middlewares;
+using PicPay.Infrastructure.Persistence;
+using PicPay.Infrastructure.Persistence.Repository;
 using Scalar.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddOpenApi();
-    builder.Services.AddDbContext<DbConnection>(opt => 
-        opt.UseNpgsql(builder.Configuration.GetConnectionString("URL")));
-    builder.Services.AddScoped<UserUc>();
-    builder.Services.AddScoped<PaymentUc>();
-
+    builder.Services.AddDbContext<DbConnection>(opt => opt.UseNpgsql(builder.Configuration["ConnectionStrings:URL"]));
+    builder.Services.AddScoped<HttpClient>();
+    builder.Services.AddScoped<IHttpServices, HttpServices>();
+    builder.Services.AddScoped<IUserRepo, UserRepo>();
+    builder.Services.AddScoped<IPaymentRepo, PaymentRepo>();
+    builder.Services.AddScoped<UserUsecase>();
+    builder.Services.AddScoped<PaymentUsecase>();
     builder.Services.AddControllers();
-    
+
     Log.Logger = new LoggerConfiguration()
         .MinimumLevel.Information()
         .Enrich.FromLogContext()
@@ -30,10 +35,11 @@ var app = builder.Build();
         app.MapScalarApiReference();
         app.MapOpenApi();
     }
+
     app.UseGlobalErrorHandler();
     app.UseHttpsRedirection();
     app.UseSerilogRequestLogging();
+    app.MapControllers();
 }
 
 app.Run();
-
