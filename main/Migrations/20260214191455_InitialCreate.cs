02aae039d030d7566e4a1ccc41394f245f0794eb
Daniel <danmoreira.dev@gmail.com>
2026-02-14 18:07:13 -0300

    feat(db): inicio das migrations

---
+++ main/Migrations/20260214191455_InitialCreate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PicPay.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "userSchema",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Cpf = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userSchema", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactionSchema",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    FkPayer = table.Column<string>(type: "text", nullable: false),
+                    FkPayee = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserSchemaId = table.Column<string>(type: "text", nullable: true),
+                    UserSchemaId1 = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactionSchema", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_transactionSchema_userSchema_UserSchemaId",
+                        column: x => x.UserSchemaId,
+                        principalTable: "userSchema",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_transactionSchema_userSchema_UserSchemaId1",
+                        column: x => x.UserSchemaId1,
+                        principalTable: "userSchema",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_UserSchemaId",
+                table: "transactionSchema",
+                column: "UserSchemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_UserSchemaId1",
+                table: "transactionSchema",
+                column: "UserSchemaId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactionSchema");
+
+            migrationBuilder.DropTable(
+                name: "userSchema");
+        }
+    }
+}
---
+++ main/Migrations/20260214194923_AddUserTransactionRelation.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PicPay.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTransactionRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transactionSchema_userSchema_UserSchemaId",
+                table: "transactionSchema");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_transactionSchema_userSchema_UserSchemaId1",
+                table: "transactionSchema");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transactionSchema_UserSchemaId",
+                table: "transactionSchema");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transactionSchema_UserSchemaId1",
+                table: "transactionSchema");
+
+            migrationBuilder.DropColumn(
+                name: "UserSchemaId",
+                table: "transactionSchema");
+
+            migrationBuilder.DropColumn(
+                name: "UserSchemaId1",
+                table: "transactionSchema");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_FkPayee",
+                table: "transactionSchema",
+                column: "FkPayee");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_FkPayer",
+                table: "transactionSchema",
+                column: "FkPayer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transactionSchema_userSchema_FkPayee",
+                table: "transactionSchema",
+                column: "FkPayee",
+                principalTable: "userSchema",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transactionSchema_userSchema_FkPayer",
+                table: "transactionSchema",
+                column: "FkPayer",
+                principalTable: "userSchema",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transactionSchema_userSchema_FkPayee",
+                table: "transactionSchema");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_transactionSchema_userSchema_FkPayer",
+                table: "transactionSchema");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transactionSchema_FkPayee",
+                table: "transactionSchema");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transactionSchema_FkPayer",
+                table: "transactionSchema");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserSchemaId",
+                table: "transactionSchema",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserSchemaId1",
+                table: "transactionSchema",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_UserSchemaId",
+                table: "transactionSchema",
+                column: "UserSchemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactionSchema_UserSchemaId1",
+                table: "transactionSchema",
+                column: "UserSchemaId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transactionSchema_userSchema_UserSchemaId",
+                table: "transactionSchema",
+                column: "UserSchemaId",
+                principalTable: "userSchema",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transactionSchema_userSchema_UserSchemaId1",
+                table: "transactionSchema",
+                column: "UserSchemaId1",
+                principalTable: "userSchema",
+                principalColumn: "Id");
+        }
+    }
+}
